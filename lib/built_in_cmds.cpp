21dfe8501a6c624c9fe8a8f467fe2c0f37acc760
MiloWang2048 <milowang2048@foxmail.com>
2020-04-10 20:21:19 +0800

    add ls

--- lib/built_in_cmds.cpp
+++ lib/built_in_cmds.cpp
@@ -1,7 +1,10 @@
+#include <dirent.h>
+
 #include "meshell.h"
 
 MeshStatus mesh_cd(MeshConfig* config, Args args);
 MeshStatus mesh_echo(MeshConfig* config, Args args);
+MeshStatus mesh_ls(MeshConfig* config, Args args);
 
 MeshStatus built_in_cmds(MeshConfig* config, Args args) {
   string cmd = args[0];
@@ -9,6 +12,8 @@ MeshStatus built_in_cmds(MeshConfig* config, Args args) {
     return mesh_cd(config, args);
   } else if (cmd == "echo") {
     return mesh_echo(config, args);
+  } else if (cmd == "ls") {
+    return mesh_ls(config, args);
   }
   return CMD_FALLTHROUGH;
 }
@@ -29,5 +34,18 @@ MeshStatus mesh_echo(MeshConfig* config, Args args) {
   for (int i = 1; i < args.size(); i++) {
     cout << args[i] << endl;
   }
+  return CMD_CAPTURED;
+}
+
+MeshStatus mesh_ls(MeshConfig* config, Args args) {
+  DIR* dir;
+  struct dirent* ptr;
+  int i;
+  const char* path = (args.size() == 1) ? getcwd(NULL, 0) : args[1].c_str();
+  dir = opendir(path);
+  while ((ptr = readdir(dir)) != NULL) {
+    printf("%s\n", ptr->d_name);
+  }
+  closedir(dir);
   return CMD_CAPTURED;
 }
