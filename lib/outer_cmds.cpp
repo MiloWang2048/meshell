feda40b1ef99575913786f8420e44fae04861b89
MiloWang2048 <milowang2048@foxmail.com>
2020-04-16 21:51:00 +0800

    optimize errmsg

--- lib/outer_cmds.cpp
+++ lib/outer_cmds.cpp
@@ -12,11 +12,11 @@ CommandStatus outer_cmds(MeshConfig* config, Args args) {
     // child process
     //printf("child:%d\n", getpid());
     execvp(args[0].c_str(), argMatrix);
-    puts("child process error");
+    puts("command not fount");
     exit(-1);
     //exit(0);
   } else if (pid < 0) {
-    puts("error: cannot vfork.");
+    puts("error: cannot fork.");
   } else {
     // parent process
     int res;
